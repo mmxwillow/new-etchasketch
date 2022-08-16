2892c279bb04160e9a9f9868d0ee8b58ff62865c
mmxwillow <mxwierzba@gmail.com>
2022-08-16 11:27:15 +0200

    Random pastel color on mouseover

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('.container');
 
-for (let j = 0; j < 16*16; j++) {
+for (let j = 0; j < 16 * 16; j++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', () => {
+        let r = Math.floor(Math.random() * (255 - 100)) + 100;
+        let g = Math.floor(Math.random() * (255 - 100)) + 100;
+        let b = Math.floor(Math.random() * (255 - 100)) + 100;
+        square.style.background = `rgb(${r},${g},${b})`;
+    })
 
     container.appendChild(square);
 }
+
